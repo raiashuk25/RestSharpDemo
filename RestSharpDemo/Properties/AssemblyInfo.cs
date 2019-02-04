0aeca9290325588cffd7c70357244cd3f507df18
ak <raiashutech@gmail.com>
2019-02-04 06:52:34 -0600

    Add project files.

---
+++ RestSharpDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RestSharpDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RestSharpDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed380b5d-5f38-4461-bf7f-c0493cf3a54d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RestSharpDemo/UnitTest1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+using System.Net;
+using System.Web;
+using RestSharpDemo.Model;
+
+namespace RestSharpDemo
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        //Test 1, write the Json output in console
+        [Test]
+        public void GetFirstPost()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid",1);
+            if (client.Execute(request).StatusCode.ToString()=="OK")
+            {
+                Console.Write(client.Execute(request).Content);
+            }
+        }
+        //Test 2, use serialization for data to get in good format like key value
+        [Test]
+        public void GetFirstPostInKeyValue()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+            var deserialize = new JsonDeserializer();
+            var outPut = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var authorName = outPut["author"];
+            Assert.That(authorName, Is.EqualTo("Ashutosh1"), "Author  name is not correct");
+        }
+        //Use of JObject to parse the Json data and validate certain values
+        [Test]
+        public void UseNewtonSoftToGetData()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}", Method.GET);
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);            
+            JObject obs = JObject.Parse(response.Content);
+            Console.Write(obs);//Just see what is printed
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Ashutosh1"), "Author  name is not correct");
+        }
+        //Post method by ananymous body class
+        [Test]
+        public void PostingDataByAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}/profile", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { name = "Ravi"});
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["name"].ToString(), Is.EqualTo("Ravi"), "Name is not correct");
+        }
+        [Test]
+        public void PostingDataByTypeBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts() { id =4, title="NewTitle4", author="Ashutosh4"});
+            var response = client.Execute(request);
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["author"].ToString(), Is.EqualTo("Ashutosh4"),"Name is not correct");
+        }
+        [Test]
+        public void UsingGenericDeserializer()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts", Method.POST);
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts() { id = 5, title = "NewTitle5", author = "Ashutosh5" });
+            var response = client.Execute<Posts>(request);
+            Assert.That(response.Data.author, Is.EqualTo("Ashutosh5"), "Name is not correct");
+        }
+
+
+    }
+
+}
